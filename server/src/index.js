ceadab0c4c32994a81933ae16ddc61724f025c60
Prabhat Singh <85447224+prabhat8701@users.noreply.github.com>
2024-07-30 23:54:07 +0530

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -5,7 +5,12 @@ dotenv.config({
     path: './.env'
 })
 
-
+const corsOrigin ={
+    origin:'http://localhost:3000', //or whatever port your frontend is using
+    credentials:true,            
+    optionSuccessStatus:200
+}
+app.use(cors(corsOrigin));
 connectDB()
 .then(() => {
     app.listen(process.env.PORT || 8000, () => {
